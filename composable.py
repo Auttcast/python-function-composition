0d74d7c5ed5d5a1e134ab9158bd13ef0a9be108b
autt <foo@bar.com>
2025-01-27 05:59:58 -0600

    reworking chain

--- composable.py
+++ composable.py
@@ -1,22 +1,60 @@
 from typing import Callable
 from collections.abc import Iterable
 
+#RULES:
+#calling func(*args) requires args to be a collection
+#all args received from __call__ will be wrapped in (,)
+#note, the call chain will be a mixture of composers and user-lambdas, with a mixture of requirements
+#results from user f or g should be wrapped in (,) as a form of normalization
+
 class Composable:
 
   def __init__(self, func, nextFunc=None, parentComp=None):
-    self.isChained = not parentComp is None
     self.parentComp = parentComp
-    self.f: Callable[[()], ()] = func
-    self.g: Callable[[()], ()] = nextFunc
+    self.f = func
+    self.g = nextFunc
+#    self.f: Callable[[()], ()] = func
+#    self.g: Callable[[()], ()] = nextFunc
     
-  def __call__(self, *args, **extra):
-    if self.isChained and self.g is None: return args
-    fr = self.f( *args )
-    if self.g is None: return fr #no composition
-    garg = fr if isinstance(fr, Iterable) and not type(fr) is type("") else [a for a in [fr]]
-    result = self.g( *fr )
+  def log(self, message):
+    if True:
+      print(f"DEBUG {message}")
+    
+  def isMain(self): return self.parentComp == None
+    
+  def _invoke(self, func, name, args):
+    funcType = "compose" if type(func) == type(self) else "func"
+    #self.log(f"BEFORE {funcType} {name}({args}) |||  {func.__hash__()}")
+    
+    r = func(*args) 
+    
+    if not type(func) == type(self):
+      r = (r,)
+      self.log(f"AFTER  {funcType} {name}({args}) -> {r}  ||| hash{func.__hash__()} ")
+    
+    return r
+  
+  def _internal_call(self, args):
+    fr = self._invoke(self.f, "f", args)
+    result = self._invoke(self.g, "g", fr) if not self.g is None else fr  
     return result
-
+    
+  def __call__(self, *args):
+    try:
+      self.log(f"________ isMain: {self.isMain()} g: {self.parentComp}")
+      
+      r = self._internal_call(args)
+      
+      if self.isMain():
+        self.log(f"UNPACKING {r}")
+        #return r #unpack args on last call
+      r = r
+      self.log(f"__call__ RETURN: {r} {type(r)} has g: {not self.g is None}")
+      return r
+    except Exception as inst:
+      self.log(f"EXCEPTION -------- {type(inst)} {inst}")
+      raise inst
+      
   def __or__(self, other):
     if isinstance(other, Composable):
         return Composable(self, other.f, parentComp=self)
--- test_composable.py
+++ test_composable.py
@@ -9,41 +9,57 @@ withStr = f(lambda x: (x, str(x)))
 strLen = f(lambda x,y: len(y))
 passthru = f(lambda x: x)
 
-def test_minimal_single_param():
-  assert inc(1) == 2
+def test_debug():
   
-def test_basic_comp():
-  inc2 = inc | inc
-  assert inc2(1) == 3
+  inc1 = f(lambda x: x+1)
+  inc2 = f(lambda x: x+2)
+  inc3 = f(lambda x: x+3)
+  inc4 = f(lambda x: x+4)
+  inc5 = f(lambda x: x+5)
   
-def test_long_comp():
-  inc5 = inc | inc | inc | inc | inc
-  assert inc5(1) == 6
-  
-def test_multi_param():
-  incPass4 = incPass | incPass | incPass | incPass
-  (r0, r1) = incPass4(1, 1)
-  assert r0 == 5 and r1 == 5
-  
-def test_various_param():
-  func = incPass | power | withStr | strLen
-  assert func(3, 3) == 3
-  
-def rangeFactory(x):
-  for i in range(1, x):
-    yield i
-  
-def test_collections():
-  pass2 = passthru | passthru | passthru
-  assert pass2([1, 2, 3]) == [1, 2, 3]
-  
-def te_iterables2():
-  rf = f(rangeFactory)
-  evens = f(lambda r: filter(lambda x: math.fmod(x, 2)[1] == 0, r))
-  toList = f(list)
-  avg = f(lambda r: sum(r) / len(r))
-  
-  func = rf | evens | toList | avg
-  
-  assert func(10) == 5
-  
+  func = inc1 | inc2 | inc3 | inc4 | inc5
+  assert func(1) == 16
+
+  def test_minimal_single_param():
+    print("DEBUG FUNC:::::: test_minimal_single_param")
+    assert inc(1) == 2
+
+  def test_basic_comp():
+    print("DEBUG FUNC:::::: test_basic_comp")
+    inc2 = inc | inc
+    assert inc2(1) == 3
+
+  def test_long_comp():
+    print("DEBUG FUNC:::::: test_long_comp")
+    inc5 = inc | inc | inc | inc
+    assert inc5(1) == 6
+
+  def test_multi_param():
+    print("DEBUG FUNC:::::: test_multi_param")
+    incPass4 = incPass | incPass | incPass | incPass
+    (r0, r1) = incPass4(1, 1)
+    assert r0 == 5 and r1 == 5
+    
+  def test_various_param():
+    print("DEBUG FUNC:::::: test_various_param")
+    func = incPass | power | withStr | strLen
+    assert func(3, 3) == 3
+    
+  def rangeFactory(x):
+    for i in range(1, x):
+      yield i
+    
+  def test_collections():
+    pass2 = passthru | passthru | passthru
+    assert pass2([1, 2, 3]) == [1, 2, 3]
+    
+  def te_iterables2():
+    rf = f(rangeFactory)
+    evens = f(lambda r: filter(lambda x: math.fmod(x, 2)[1] == 0, r))
+    toList = f(list)
+    avg = f(lambda r: sum(r) / len(r))
+    
+    func = rf | evens | toList | avg
+    
+    assert func(10) == 5
+    
