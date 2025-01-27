6f947c3b3cfbc95a6309187826c27f28ac5dd286
autt <foo@bar.com>
2025-01-27 00:55:40 -0600

    testing

--- composable.py
+++ composable.py
@@ -1,28 +1,7 @@
 from typing import Callable
 from collections.abc import Iterable
 
-#todo
-#github?
-#unit test cases:
-#various output/input types and param args
-#using the same function in multiple chains
-#exception handling
-#chains which range in number of parameters
-#can it support memoization?
-
-#Description
-#This is a simple class that overrides the | operator allowing for cleaner composition
-#How does it work?
-#incFunc = f(lambda x: x+1)
-#addThree = incFunc | incFunc | incFunc
-#r = addThree(5)
-#the result is 5.
-#The Comp class applies what is necessary to compose function calls together with pipe |
-#The Comp class itself is also considered an executable lambda
-#When a composition is created with f | g, a new Comp object is return which simply contains pointers to either function
-#If additional functions are chained, the pointers are updated.
-
-class Comp:
+class Composable:
 
   def __init__(self, func, nextFunc=None, parentComp=None):
     self.isChained = not parentComp is None
@@ -32,15 +11,15 @@ class Comp:
     
   def __call__(self, *args, **extra):
     if self.isChained and self.g is None: return args
-    c1 = self.f( *args )
-    if self.g is None: return c1 #no composition
-    garg = c1 if isinstance(c1, Iterable) and not type(c1) is type("") else [a for a in [c1]]
-    result = self.g( *garg )
+    fr = self.f( *args )
+    if self.g is None: return fr #no composition
+    garg = fr if isinstance(fr, Iterable) and not type(fr) is type("") else [a for a in [fr]]
+    result = self.g( *fr )
     return result
 
   def __or__(self, other):
-    if isinstance(other, Comp):
-        return Comp(self, other.f, parentComp=self)
+    if isinstance(other, Composable):
+        return Composable(self, other.f, parentComp=self)
     else:
-        raise TypeError(f"Unsupported operand type(s) for |: 'Comp' and '{type(other).__name__)}'")
+        raise TypeError(f"Unsupported operand type(s) for |: 'Composable' and '{type(other).__name__}'")
 
---
+++ test_composable.py
@@ -0,0 +1,49 @@
+import math, composable
+
+f = composable.Composable
+
+inc = f(lambda x: x+1)
+incPass = f(lambda x,y: (x+1, y+1))
+power = f(lambda x,y: x**y)
+withStr = f(lambda x: (x, str(x)))
+strLen = f(lambda x,y: len(y))
+passthru = f(lambda x: x)
+
+def test_minimal_single_param():
+  assert inc(1) == 2
+  
+def test_basic_comp():
+  inc2 = inc | inc
+  assert inc2(1) == 3
+  
+def test_long_comp():
+  inc5 = inc | inc | inc | inc | inc
+  assert inc5(1) == 6
+  
+def test_multi_param():
+  incPass4 = incPass | incPass | incPass | incPass
+  (r0, r1) = incPass4(1, 1)
+  assert r0 == 5 and r1 == 5
+  
+def test_various_param():
+  func = incPass | power | withStr | strLen
+  assert func(3, 3) == 3
+  
+def rangeFactory(x):
+  for i in range(1, x):
+    yield i
+  
+def test_collections():
+  pass2 = passthru | passthru | passthru
+  assert pass2([1, 2, 3]) == [1, 2, 3]
+  
+def te_iterables2():
+  rf = f(rangeFactory)
+  evens = f(lambda r: filter(lambda x: math.fmod(x, 2)[1] == 0, r))
+  toList = f(list)
+  avg = f(lambda r: sum(r) / len(r))
+  
+  func = rf | evens | toList | avg
+  
+  assert func(10) == 5
+  
