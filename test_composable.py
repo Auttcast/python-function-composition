f20f380bd129439ae1bc3336a3941a410265edd1
autt <foo@bar.com>
2025-01-27 14:04:33 -0600

    tests are passing

--- test_composable.py
+++ test_composable.py
@@ -53,27 +53,27 @@ def test_multi_param():
   incPass4 = incPass | incPass | incPass | incPass
   (r0, r1) = incPass4(1, 1)
   assert r0 == 5 and r1 == 5
+
+def test_various_param():
+  print("DEBUG FUNC:::::: test_various_param")
+  func = incPass | power | withStr | strLen
+  assert func(3, 3) == 3
+
+def rangeFactory(x):
+  for i in range(1, x):
+    yield i
   
-  def test_various_param():
-    print("DEBUG FUNC:::::: test_various_param")
-    func = incPass | power | withStr | strLen
-    assert func(3, 3) == 3
-    
-  def rangeFactory(x):
-    for i in range(1, x):
-      yield i
-    
-  def test_collections():
-    pass2 = passthru | passthru | passthru
-    assert pass2([1, 2, 3]) == [1, 2, 3]
-    
-  def te_iterables2():
-    rf = f(rangeFactory)
-    evens = f(lambda r: filter(lambda x: math.fmod(x, 2)[1] == 0, r))
-    toList = f(list)
-    avg = f(lambda r: sum(r) / len(r))
-    
-    func = rf | evens | toList | avg
-    
-    assert func(10) == 5
-    
+def test_collections():
+  pass2 = passthru | passthru | passthru
+  assert pass2([1, 2, 3]) == [1, 2, 3]
+
+def test_iterables():
+  rf = f(rangeFactory)
+  evens = f(lambda r: filter(lambda x: x % 2 == 0, r))
+  toList = f(lambda r: list(r))
+  avg = f(lambda r: sum(r) / len(r))
+  
+  func = rf | evens | toList | avg
+  
+  assert func(10) == 5
+  
