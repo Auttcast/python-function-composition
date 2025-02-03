85a062ce03f40fd48c207fad16a4745b06fe1c7c
autt <foo@bar.com>
2025-02-03 00:09:39 -0600

    switching to frame utils

--- auttcomp/queryBuilder.py
+++ auttcomp/queryBuilder.py
@@ -2,66 +2,97 @@ from collections.abc import Callable
 from .shapeEval import evalShape
 from .quicklog import log
 
+import sys
+
+printTracking = True
+
 class Ghost(object):
-  def __init__(self):
+  def __init__(self, id=1):
     super().__init__()
     self.tracking = []
+    self.id = id
 
   def __repr__(self):
     return "****SELF****"
 
+
+
   def __getattr__(self, name):
-    print(f"__getattr__ {name}")
+    if printTracking: print(f"{self.id} __getattr__ {name}")
     self.tracking.append([name])
+    if printTracking: print(self.tracking)
     return self
 
   def __getitem__(self, index):
-    print(f"__getitem__ {index}")
-    return self
+    if printTracking: print(f"{self.id} __getitem__ {index}")
+    self.tracking.append(index.tracking)
+    return None
 
-  def __gt__(self, other):
-    print(f"__gt__ {other}")
-    self.tracking.append(('>', other))
-    return self
 
-  def __lt__(self, other):
-    print(f"__lt__ {other}")
-    self.tracking.append(('<', other))
-    return self
+
 
   def __index__(self):
     print("index")
 
+  def __bool__(self):
+    print(f"__bool__")
+    return True
+    raise Exception("invalid operation - not allowed to return anything other than bool")
+
+  def __iter__(self):
+    print('iter')
+
+  def __contains__(self, item):
+    print(f"__contains__ {item}")
+    #raise Exception("invalid operation - requires bool return")
+
+    return True
+
+  def __call__(self, *args, **kwargs):
+    print(f"__call__ {args}")
+    return self.tracking
+
+
+
   def __eq__(self, other):
     print(f"__eq__ {('==', other)}")
-    self.tracking.append(('==', other))
+    self.tracking[-1:][0].append(('==', other))
+    return other
+
+  def __ne__(self, other):
+    print(f"__ne__ {('!=', other)}")
+    self.tracking[-1:][0].append(('!=', other))
     return other
 
   def __and__(self, other):
     print(f"__and__ {other}")
     self.tracking.append(('and', other(self.tracking)))
-    return self
-
-  def __bool__(self):
-    print(f"__bool__")
-    raise Exception("invalid operation - not allowed to return anything other than bool")
-
-  def __call__(self, *args, **kwargs):
-    print(f"__call__")
-    return self.tracking
+    return other
 
   def __or__(self, other):
     print(f"__or__ {other}")
-    self.tracking.append(('or', other))
-    return self
+    self.tracking.append(('or', other(self.tracking)))
+    return other
 
-  def __contains__(self, item):
-    print(f"__contains__ {item}")
-    raise Exception("invalid operation - requires bool return")
+  def __gt__(self, other):
+    print(f"__gt__ {other}")
+    self.tracking[-1:][0].append(('>', other))
+    return other
+
+  def __lt__(self, other):
+    print(f"__lt__ {other}")
+    self.tracking[-1:][0].append(('<', other))
+    return other
 
   def __add__(self, other):
     print(f"__add__ {other}")
     self.tracking.append(('add', other(self.tracking)))
-    return self
+    return other
 
   #def __rshift__(self, other):
+
+
+def select(func):
+  g = Ghost()
+  r = func(g)
+  return r.tracking
--- auttcomp/testing/test_queryBuilder.py
+++ auttcomp/testing/test_queryBuilder.py
@@ -1,25 +1,84 @@
-import traceback
-
 from ..queryBuilder import *
 from ..quicklog import *
+
+
 '''
-
-
 identity:   lambda x: x -> []
 column:     lambda x: x[x.name] -> [[name]]
 multi colum lambda x: x[x.name, x.foo] -> [[name, foo]]
 
-
-'''
-@tracelog("test_query_builder1", enable=True)
-def test_query_builder1():
-
-  def complexType():
-    func2 = lambda x: (x.
+func2 = lambda x: (x.
                       models[x.left == "allen watts left", x.right == "allen watts right"] #where
                       .authorData[x.fullname1,x.fullname2, x.fullname3] #select
                       )
-    r = func2(Ghost())
-    print(list(r.tracking))
-  complexType()
-  # s = slice(0, 3, -2)
+
+
+
+
+
+
+'''
+
+isLogging = False
+
+###SELECTION
+
+@tracelog("test_query_builder_identity", enable=isLogging)
+def test_query_builder_identity():
+  func = lambda x: x
+  r = select(func)
+  assert r == []
+
+@tracelog("test_query_builder_property", enable=isLogging)
+def test_query_builder_property():
+  func = lambda x: x.model.FOO.bar
+  r = select(func)
+  assert r == [['model'], ['FOO'], ['bar']]
+
+@tracelog("test_query_builder_property_arr", enable=isLogging)
+def xtest_query_builder_property_arr():
+  func = lambda x: x.model[x.FOO.bar]
+  r = select(func)
+  print(r)
+  assert r == [['model', [['FOO'], ['bar']]]]
+
+
+
+
+
+
+
+
+
+# TODO
+
+@tracelog("test_query_builder_property_condition", enable=isLogging)
+def xtest_query_builder_property_condition():
+  func = lambda x: x.model[x.prop == 1]
+  r = func(Ghost()).tracking
+  print(r)
+  assert r == [['model'], ['prop', ('==', 1)]]
+
+@tracelog("test_query_builder_property_condition2", enable=isLogging)
+def xtest_query_builder_property_condition2():
+  func = lambda x: x.model[x.prop1 == 1, x.prop2 == 2]
+  r = func(Ghost()).tracking
+  print(r)
+  assert r == [['model'], ['prop1', ('==', 1)], ['prop2', ('==', 2)]]
+
+@tracelog("test_query_builder_property_condition2_and", enable=isLogging)
+def xtest_query_builder_property_condition2_and():
+  func = lambda x: x.model[x.prop1 == True, x.prop2 != 2, x.prop2 != x.prop3]
+  r = func(Ghost()).tracking
+  print(r)
+  assert r == [['model'], ['prop1', ('==', 1)]]
+
+
+
+
+@tracelog("test_query_builder_property_condition_in", enable=isLogging)
+def ztest_query_builder_property_condition_in():
+  func = lambda x: x.model[x.prop in x[[1, 2]]]
+  r = func(Ghost()).tracking
+  print(r)
+  assert r == [['model'], ['prop', ('==', 1)], ['prop', ('==', 2)]]
