6c0017697ed10563deba314fc52b8516837c22d4
autt <foo@bar.com>
2025-02-02 20:14:52 -0600

    cleanup

--- auttcomp/queryBuilder.py
+++ auttcomp/queryBuilder.py
@@ -1,126 +1,67 @@
 from collections.abc import Callable
-from types import SimpleNamespace
-
 from .shapeEval import evalShape
 from .quicklog import log
 
-
-
-class OwnedList(list):
-  def __init__(self, owner):
+class Ghost(object):
+  def __init__(self):
     super().__init__()
     self.tracking = []
 
-  def __setitem__(self, key, value):
-    if isinstance(value, Ghost): return
-    self.tracking.append(value)
-
-  def __iadd__(self, other):
-    if isinstance(other, Ghost): return
-    self.tracking.append(other)
-
-  def __radd__(self, other):
-    if isinstance(other, Ghost): return
-    self.tracking.append(other)
-
-  def append(self, value):
-    if isinstance(value, Ghost): return
-    self.tracking.append(value)
-
-  def insert(self, idx, value):
-    if isinstance(value, Ghost): return
-    self.tracking.insert(idx, value)
-
-
-
-
-
-class Ghost(object):
-  def __init__(self, tracking=None):
-    super().__init__()
-
-    if tracking is None:
-      self.tracking = OwnedList(self)
-    else:
-      self.tracking = tracking
-
-  def logPropAccess(self, message):
-    if True:
-      log(message)
+  def __repr__(self):
+    return "****SELF****"
 
   def __getattr__(self, name):
-    return Ghost(self.tracking + [name])
-
-  def __setattr__(self, key, value):
-    print(f"SET:::::: {key} value: {value} inst: {type(value)}")
-    if isinstance(value, Ghost): return
-    super().__setattr__(key, value)
-
-  @staticmethod
-  def __getRootContainer(obj, parent=None):
-    if isinstance(obj, Ghost): return type(parent) if parent is not None else None
-    if not isinstance(obj, Ghost): return Ghost.__getRootContainer(obj[0], obj)
-
-  @staticmethod
-  def __prepareAndEval(obj):
-    if isinstance(obj, Ghost):
-      return evalShape(obj.tracking, setAnyType=True)
-    else:
-      return evalShape(obj, setAnyType=True)
+    print(f"__getattr__ {name}")
+    self.tracking.append([name])
+    return self
 
   def __getitem__(self, index):
-    if index == self: return self
-    self.logPropAccess(f"__gt__ {self.tracking} {index}")
-    shape = Ghost.__prepareAndEval(index)
-    print(f"SHAPE:::: {shape}")
-
-    if isinstance(index, Ghost): # [x.foo == "bar"]
-      shapes = [
-        ['Any', ('Any',)],
-        ({'tracking': ['Any', ('Any',)]},)
-      ]
-
-      self.tracking.append([index[x].tracking[0] for x in range(0, len(index))])
-    elif isinstance(index, Callable): # [lambda x.foo == "bar"]
-      self.tracking.append(index(Ghost()).tracking)
-
-    else: # [x.foo, x.bar, x.baz]
-      shapes = [
-        ({'tracking': ['Any']},)
-      ]
-
-      self.tracking.append([index[x].tracking[0] for x in range(0, len(index))])
+    print(f"__getitem__ {index}")
     return self
 
   def __gt__(self, other):
+    print(f"__gt__ {other}")
     self.tracking.append(('>', other))
     return self
 
   def __lt__(self, other):
+    print(f"__lt__ {other}")
     self.tracking.append(('<', other))
     return self
 
+  def __index__(self):
+    print("index")
+
   def __eq__(self, other):
+    print(f"__eq__ {('==', other)}")
     self.tracking.append(('==', other))
-    return self
+    return other
 
   def __and__(self, other):
+    print(f"__and__ {other}")
     self.tracking.append(('and', other(self.tracking)))
     return self
 
   def __bool__(self):
+    print(f"__bool__")
     raise Exception("invalid operation - not allowed to return anything other than bool")
 
   def __call__(self, *args, **kwargs):
+    print(f"__call__")
     return self.tracking
 
   def __or__(self, other):
+    print(f"__or__ {other}")
     self.tracking.append(('or', other))
     return self
 
   def __contains__(self, item):
+    print(f"__contains__ {item}")
     raise Exception("invalid operation - requires bool return")
 
   def __add__(self, other):
+    print(f"__add__ {other}")
     self.tracking.append(('add', other(self.tracking)))
     return self
+
+  #def __rshift__(self, other):
--- auttcomp/testing/test_queryBuilder.py
+++ auttcomp/testing/test_queryBuilder.py
@@ -1,3 +1,5 @@
+import traceback
+
 from ..queryBuilder import *
 from ..quicklog import *
 '''
@@ -12,51 +14,12 @@ multi colum lambda x: x[x.name, x.foo] -> [[name, foo]]
 @tracelog("test_query_builder1", enable=True)
 def test_query_builder1():
 
-  #tabular data
-  #[['id', 'name', 'age']]
-  def tabulartype():
-    tabParam = Ghost()
-    func1 = lambda x: x[x.id, x.name, x.age]
-
-    tr = func1(tabParam)
-    print(tr.tracking)
-
-  #lambda: ['models', ['author', ('==', 'allen watts')], 'authorData', ['fullname', 'type', 'followerCount']]
-  #tuple:  ['models', ['author', ('==', 'allen watts')], 'authorData', ['fullname', 'type', 'followerCount']]
-
-  #  access         filter                            access               select
-  #['models', ['author', ('==', 'allen watts')], 'authorData', ['fullname', 'type', 'followerCount']]
   def complexType():
-    complexParam = Ghost()
-    #['author', ('==', 'allen watts')]
     func2 = lambda x: (x.
-                      models[x.left == "allen watts left"] #where
-                      .authorData[x.fullname, x.fullname] #select
+                      models[x.left == "allen watts left", x.right == "allen watts right"] #where
+                      .authorData[x.fullname1,x.fullname2, x.fullname3] #select
                       )
-    r = func2(complexParam)
-    print(r.tracking)
+    r = func2(Ghost())
+    print(list(r.tracking))
   complexType()
   # s = slice(0, 3, -2)
-
-  #using index accessor to filter
-  #func = lambda x: x.models[where(lambda x: x.author == "allen watts")].authorData.followerCount
-  #note slice is sealed
-
-  #func = lambda x: x.models[where(lambda x: x.author == "allen watts")].authorData
-
-
-  # {} [] {} prop
-  # get map get get
-  # flatmap for each [] in path
-  # where x.models.authorData.downloads > 10000
-  #graph monad / chain
-  # graph(query) -> monad; filter, etc; -> project
-  # conditional api:
-  # - must separate path from evaluation
-  # - .graphWhere(lambda x.models.authorData.downloads, lambda x: x >
-  #
-
-  #
-  # r = func(Foo())
-  # print(r.tracking)
-  #print(r.tracking[5][1])
