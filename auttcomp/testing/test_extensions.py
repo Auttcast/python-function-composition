a135a6aafe2909290e025917d354cfb42972962e
autt <foo@bar.com>
2025-04-03 16:38:04 -0500

    starting test_exp_async

--- auttcomp/testing/test_extensions.py
+++ auttcomp/testing/test_extensions.py
@@ -1,9 +1,3 @@
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
-import time
-from typing import Any, Callable, Iterable, ParamSpec, TypeVar
-
-from auttcomp.composable import Composable
 from ..extensions import KeyValuePair, Api as f
 from .base_test import get_hugging_face_sample
 
@@ -295,68 +289,3 @@ def test_huggingface_sample():
     )
 
     assert result == [('black-forest-labs', 1548084), ('deepseek-ai', 1448374), ('microsoft', 264891)]
-
-def test_exp_parallel():
-
-    def callback(x):
-        time.sleep(1)
-        return x + 1
-    
-    def is_even(x):
-        time.sleep(1)
-        return x % 2 == 0
-
-    data = [1, 2, 3]
-        
-    def id_param(x:Any) -> Any:
-        return x
-
-    P = ParamSpec('P')
-    R = TypeVar('R')
-
-    class ParallelApi:
-
-        def __init__(self, pool:ThreadPoolExecutor):
-            self.pool:ThreadPoolExecutor = pool
-
-        def map[T, R](self, func: Callable[[T], R]) -> Callable[[Iterable[T]], Iterable[R]]:
-            
-            @Composable
-            def partial_map(data: Iterable[T]) -> Iterable[R]:
-                return self.pool.map(func, data)
-
-            return partial_map
-
-        def filter[T, R](self, func: Callable[[T], R] = id_param) -> Callable[[Iterable[T]], Iterable[T]]:
-            
-            @Composable
-            def partial_filter(data: Iterable[T]) -> Iterable[T]:
-                lift = self.pool.map(lambda x: (x, func(x)), data)
-                lift_filter = filter(lambda x: x[1], lift)
-                lift_return = map(lambda x: x[0], lift_filter)
-                return lift_return
-
-            return partial_filter
-        
-        def list[T](self, data: Iterable[T]) -> list[T]:
-            return list(data)
-
-
-    class ParallelContext:
-        def __init__(self, pool:ThreadPoolExecutor):
-            self.pool:ThreadPoolExecutor = pool
-
-        def __call__(self, composition:Callable[[ParallelApi], Composable]) -> Composable:
-            return composition(ParallelApi(self.pool))
-
-    with ThreadPoolExecutor() as pool:
-
-        result = f.id(data) > ParallelContext(pool)(lambda f: (
-            f.map(callback) 
-            | f.map(callback)
-            | f.map(callback)
-            | f.filter(is_even)
-            | f.list
-        ))
-
-        print(result)
