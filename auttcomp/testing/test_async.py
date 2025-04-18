3e967ef4980714b962d35a4254eb3c0c96548a49
autt <foo@bar.com>
2025-04-18 04:02:35 -0500

    async eager exec 1

--- auttcomp/testing/test_async.py
+++ auttcomp/testing/test_async.py
@@ -1,4 +1,4 @@
-from typing import Any, AsyncGenerator, Callable, Iterable
+from typing import Any, AsyncGenerator, Awaitable, Callable, Coroutine, Iterable, Union
 from auttcomp.async_composable import AsyncComposable
 from ..extensions import Api as f
 import asyncio
@@ -6,49 +6,111 @@ import pytest
 
 class AsyncApi:
 
+    '''
+    Pattern to optimize for parallelism and eager execution:
+    aggregators and terminators(like list), should exec all tasks from the async gen first,
+    
+    reinforce a best practice: when async is used, everything should be async.
+    -not going to worry about async-sync type of issues here, except for lambdas
+
+    todo:
+    -bug fix missing f.list
+    -consider custom AsyncApi.iter which would support the execution pattern, but adapt to iterable instead
+    -async lambda workaround
+
+
+    review
+    the eager parallel model seems powerful, however it is limited in that one cannot easily determine if yields from async_gen have been cancelled.
+    this means that applying an intermediate adapter among the higher order functions is not practical, for implementing filter per task cancellation
+
+    while I do like this execution model quite a lot, perhaps it would be best to form it into a more limited contextual fluent api
+    one that forces ordering, since .filter cannot run after .map here.
+
+    maybe it is possible with asyncio.current_task() within the exec functions ? but I don't like the idea of, for example,
+    having a huge list filtered to only a massive list, and the further continuations executing, still, a huge list of cancellations. 
+    contextual fluent sounds much more practical
+
+    tentative
+    AsyncContext will later be reworked for general purpose async
+    the current code will be labeled something like AsyncEagerContext
+
+    AsyncEagerContext outline
+    The operations for each higher order function and element of data are executed as quickly as possible.
+    This requires all higher order functions to yield a defered execution and a terminating function (like .list or some aggregate) 
+    to run the stack of compositions.
+    Consequently, filter can only be implemented as a task cancellation and further maps after filter are not practical
+    '''
+
+    @staticmethod
+    async def __co_map_exec(co_func, co):
+        return await co_func(await co)
+
     def map[T, R](self, func:Callable[[T], R]) -> Callable[[AsyncGenerator[Any, T]], AsyncGenerator[Any, R]]:
         
         @AsyncComposable
-        async def partial_map(data: AsyncGenerator[Any, T]) -> AsyncGenerator[Any, R]:
-            '''
-            fanout?
-            '''
-            
-            running = []
-            async for d in data:
-                running.append(func(d))
+        async def partial_map(source_gen: AsyncGenerator[Any, Awaitable[T]]) -> AsyncGenerator[Any, Awaitable[R]]:
+            async for co in source_gen:
+                yield AsyncApi.__co_map_exec(func, co)
                 
-            for complete in asyncio.as_completed(running):
-                yield await complete
+        return partial_map
 
+    @staticmethod
+    async def __co_filter_exec(filter_func_co, source_co):
+        value = await source_co
+        if await filter_func_co(value):
+            return value
+        asyncio.current_task().cancel()
+    
+
+    def filter[T](self, func:Callable[[T], bool]) -> Callable[[AsyncGenerator[Any, T]], AsyncGenerator[Any, T]]:
+        '''
+        filter implemented with eager execution
+        if an element is filtered, the task is canceled
+        '''
+        @AsyncComposable
+        async def partial_map(source_gen: AsyncGenerator[Any, Awaitable[T]]) -> AsyncGenerator[Any, Awaitable[T]]:
+            async for co in source_gen:
+                yield AsyncApi.__co_filter_exec(func, co)
+                
         return partial_map
 
     @staticmethod
     @AsyncComposable
-    async def list[T](data:AsyncGenerator[Any, T]) -> list[T]:
-        result = []
-        async for d in data:
-            result.append(d)
-        return result
+    async def list[T](source_gen:AsyncGenerator[Any, Awaitable[T]]) -> list[T]:
+        
+        running = []
+        async for d in source_gen:
+            t = asyncio.create_task(d)
+            running.append(t)
+
+        results = []
+        for c in running:
+            if not c.cancelled():
+                r = await c
+                results.append(r)
+
+        return results
 
 class AsyncContext:
 
-    '''
-    reinforce a best practice: when async is used, everything should be async.
-    -not going to worry about async-sync type of issues
-
-    context will lift Iterable to AsyncGenerator (if not already)
-    '''
-
     @staticmethod
     @AsyncComposable
-    async def iterable_to_async_gen[T](data:Iterable[T]) -> AsyncGenerator[Any, T]:
-        #todo match iter, gen, _ (scalar)
-        for d in data:
-            yield d
+    async def source_adapter[T](data:Union[AsyncGenerator[Any, T] | Iterable[T]]) -> AsyncGenerator[Any, T]:
+        
+        if isinstance(data, Iterable):
+            for x in data:
+                yield asyncio.sleep(0, x)
+        elif isinstance(data, AsyncGenerator):
+            async for x in data:
+                if isinstance(x, Coroutine):
+                    yield x
+                else:
+                    yield asyncio.sleep(0, x)
+        else:
+            raise TypeError(f"data type {type(data)} not supported")
 
     def __call__(self, composition_factory:Callable[[AsyncApi], AsyncComposable]) -> AsyncComposable:
-        return AsyncContext.iterable_to_async_gen | composition_factory(AsyncApi())
+        return AsyncContext.source_adapter | composition_factory(AsyncApi())
 
 @pytest.mark.asyncio
 async def test_comp_w_id_invoke():
@@ -64,94 +126,25 @@ async def test_comp_w_id_invoke():
 
     assert r == 4
 
-
-
 @pytest.mark.asyncio
 async def test_map_ext():
 
-    '''
-    prereq: test_comp_w_id_invoke
-    cancellation
-    iterable/ag - AsyncContext adapter
-
-    caveat
-    async composition
-    async gen and coroutines are not very composable because of await
-
-    option1:
-    custom awaitable async gen
-
-    '''
-
-    async def get_gen():
-        for x in range(0, 3):
-            yield x
-
     async def inc_async(x):
-        print('start')
-        await asyncio.sleep(1)
+        await asyncio.sleep(x)
         return x + 1
     
-    api = AsyncApi()
+    async def test_filter_async(x):
+        return x != 3
 
+    data = [2, 1, 2]
 
-    comp = (
-        api.map(inc_async) 
-        | api.map(inc_async) 
-        | api.list
-        )
-    result = await comp(get_gen())
-    print(result)
-
-    # data = [2, 1, 3]
-
-    # async_comp = AsyncContext()(lambda f: (
-    #     f.map(inc_async)
-    #     | f.list
-    # ))
+    async_comp = AsyncContext()(lambda f: (
+        f.map(inc_async)
+        | f.map(inc_async)
+        | f.filter(test_filter_async)
+        | f.list
+        | f.list
+    ))
         
-    # result_list = async_comp(data)
-    # print(result_list)
-    # gen = await result_list
-    # print(gen)
-    # async for x in gen:
-    #     print(x)
-    
-
-@pytest.mark.asyncio
-async def test_async_comps():
-
-    '''
-    () -> ag | (ag) -> int
-
-    just check if response is awaitable
-
-    '''
-
-    async def test2():
-        return 123
-    
-    async def test1():
-        return await test2()
-
-    print(await test1())
-
-    @AsyncComposable
-    async def get_gen() -> AsyncGenerator[Any, int]:
-        for x in range(0, 3):
-            yield x
-
-    @AsyncComposable
-    async def get_co(ag:AsyncGenerator[Any, int]) -> int:
-        await asyncio.sleep(1)
-        count = 0
-        async for x in ag:
-            count += 1
-        return count
-    
-    comp = get_gen | get_co
-
-    x = await comp()
-
-    print(x)
-
+    result = await async_comp(data)
+    print(result)
